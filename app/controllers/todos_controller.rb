5ab6f538afbafc05bc515d585122383ccd4a321f
dammytrager <damilaremalomo@gmail.com>
2022-10-06 18:47:04 +0100

    Add edit and create actions

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,7 +3,7 @@ class TodosController < ApplicationController
 
   # GET /todos or /todos.json
   def index
-    @todos = Todo.all
+    @todos = Todo.all.order(created_at: :desc)
   end
 
   # GET /todos/1 or /todos/1.json
@@ -21,50 +21,25 @@ class TodosController < ApplicationController
 
   # POST /todos or /todos.json
   def create
-    @todo = Todo.new(todo_params)
-
-    respond_to do |format|
-      if @todo.save
-        format.html { redirect_to todo_url(@todo), notice: "Todo was successfully created." }
-        format.json { render :show, status: :created, location: @todo }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
+    Todo.create(params[:todo].permit!)
+    redirect_to todos_path
   end
 
   # PATCH/PUT /todos/1 or /todos/1.json
   def update
-    respond_to do |format|
-      if @todo.update(todo_params)
-        format.html { redirect_to todo_url(@todo), notice: "Todo was successfully updated." }
-        format.json { render :show, status: :ok, location: @todo }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
+    @todo.update(params[:todo].permit!)
+    redirect_to todos_path
   end
 
   # DELETE /todos/1 or /todos/1.json
   def destroy
     @todo.destroy
-
-    respond_to do |format|
-      format.html { redirect_to todos_url, notice: "Todo was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to todos_path
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_todo
       @todo = Todo.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def todo_params
-      params.fetch(:todo, {})
     end
 end
