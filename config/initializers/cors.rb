3c6631c7fecd3b93273419bc1ebb2a4e73117d81
dammytrager <damilaremalomo@gmail.com>
2022-12-21 18:43:26 +0100

    Add cors

---
+++ config/initializers/cors.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: false, logger: (lambda {
+  Rails.logger
+}) do
+  allow do
+    origins ['*']
+
+    resource '/api/*',
+             headers: :any,
+             methods: %i[get post delete put patch]
+  end
+end
